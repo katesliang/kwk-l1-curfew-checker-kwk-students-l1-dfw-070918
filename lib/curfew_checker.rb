ae0ea5f723b07a13de56f4993f630fed3eb72270
katesliang <kateliangg@gmail.com>
2018-07-10 18:35:41 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -14,31 +14,31 @@ end
 
 def complex_curfew_checker(time)
    if time > 11
-    puts "You're in trouble! Better get home quick!"
+    "You're in trouble! Better get home quick!"
     elsif time == 11
-    puts "Time to apparate!"
+    "Time to apparate!"
   else 
-    puts "Keep having fun!"
+    "Keep having fun!"
   end
 end
 
 def deluxe_curfew_checker(time)
   if time > 11
-    puts "You're in trouble! Better get home quick!"
+    "You're in trouble! Better get home quick!"
     elsif time == 11
-    puts "Time to apparate!"
+    "Time to apparate!"
   else 
-    puts "You have #{11 - time} hour(s) left to keep having fun!"
+    "You have #{11 - time} hour(s) left to keep having fun!"
   end
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
    if current_time > 11 && curfew_time == 11
-    puts "You're in trouble! Better get back to Hogwarts quick!"
+   "You're in trouble! Better get back to Hogwarts quick!"
     elsif current_time == 11 && curfew_time == 11
-    puts "Time to apparate!"
+   "Time to apparate!"
   else 
-    puts "You have #{11 - current_time} hour(s) left to keep having fun!"
+  "You have #{11 - current_time} hour(s) left to keep having fun!"
   end
 end
 
